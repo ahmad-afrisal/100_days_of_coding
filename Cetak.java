654dab832c9984ccb8ea1cef98ca8ac7dc5c3f0e
Ahmad Afrisal <48119973+ahmad-afrisal@users.noreply.github.com>
2023-10-06 20:47:48 +0800

    Add files via upload
    Day 1, Commit ke 2

---
+++ Cetak.java
@@ -0,0 +1,11 @@
+package pertemuan2;
+
+public class Cetak {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello");
+        System.out.print("Hello");
+        System.out.println();
+    }
+}
